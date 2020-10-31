fc2c3eabc3f71272ea3e76ba79da7c845df663a2
PSDat123 <sieudat123@gmail.com>
2020-10-31 16:50:47 +0700

    Implemented merge sort (version 1)

--- JS/Methods.js
+++ JS/Methods.js
@@ -116,7 +116,184 @@ Method.prototype.isort = function(){
 	}
 	main();
 }
+//Merge Sort
+Method.prototype.msort = function (){
+	c.fillStyle = "#ffffff";
+	this.status = 1;
+	//Start Timer
+	const start = new Date().getTime();
+	console.log(`Start: ${start}`);
 
-Method.prototype.msort = function(){
-	
+	let queue = [];
+	(function merge_index(l,r){
+		if(l<r){
+			let m = Math.floor(l + (r - l) / 2);
+			merge_index(l,m);
+			merge_index(m+1, r);
+			queue.push([l,m,r]);
+		}
+	})(0, this.data.length - 1);
+	// merge_index(0, this.data.length - 1);
+	console.log(queue);
+	let q_i = 0
+	let l = queue[q_i][0];
+	let m = queue[q_i][1];
+	let r = queue[q_i][2];
+	let a1 = this.data.slice(l, m + 1);
+	let a2 = this.data.slice(m + 1, r + 1);
+	let l1 = 0, l2 = 0;
+	let main = () =>{
+		this.req = requestAnimationFrame(main);
+		if (l + l1 > m && m + 1 + l2 > r) {
+			q_i++;
+			if (q_i >= queue.length) {
+				c.fillStyle = "#00ff00";
+				this.showData();
+				cancelAnimationFrame(this.req);
+				this.status = 0;
+
+				//End Timer
+				let end = new Date().getTime();
+				console.log(`End: ${end}`);
+				console.log(`Time taken: ${end - start}ms`); //Time taken
+			}
+			else {
+				l = queue[q_i][0];
+				m = queue[q_i][1];
+				r = queue[q_i][2];
+				a1 = this.data.slice(l, m + 1);
+				a2 = this.data.slice(m + 1, r + 1);
+				l1 = 0;
+				l2 = 0;
+			}
+		}
+		else if (l + l1 > m){
+			this.data[l + l1 + l2] = [this.data[l + l1 + l2][0], a2[l2][1]];
+			this.showData();
+			//Red line
+			c.fillStyle = "#ff0000";
+			c.fillRect(this.data[l + l1 + l2][0], this.c_height - this.data[l + l1 + l2][1], this.col_w, this.data[l + l1 + l2][1]);
+			c.fillStyle = "#ffffff";
+
+			l2++;
+		}
+		else if (m + 1 + l2 > r) {
+			this.data[l + l1 + l2] = [this.data[l + l1 + l2][0], a1[l1][1]];
+			this.showData();
+			//Red line
+			c.fillStyle = "#ff0000";
+			c.fillRect(this.data[l + l1 + l2][0], this.c_height - this.data[l + l1 + l2][1], this.col_w, this.data[l + l1 + l2][1]);
+			c.fillStyle = "#ffffff";
+
+			l1++;
+		}
+		else{
+			// console.log(a1, a2);
+			if(a1[l1][1] > a2[l2][1]){
+				this.data[l + l1 + l2] = [this.data[l + l1 + l2][0], a2[l2][1]];
+				this.showData();
+				//Red line
+				c.fillStyle = "#ff0000";
+				c.fillRect(this.data[l + l1 + l2][0], this.c_height - this.data[l + l1 + l2][1], this.col_w, this.data[l + l1 + l2][1]);
+				c.fillStyle = "#ffffff";
+
+				l2++;
+			}
+			else{
+				this.data[l + l1 + l2] = [this.data[l + l1 + l2][0], a1[l1][1]];
+				this.showData();
+				//Red line
+				c.fillStyle = "#ff0000";
+				c.fillRect(this.data[l + l1 + l2][0], this.c_height - this.data[l + l1 + l2][1], this.col_w, this.data[l + l1 + l2][1]);
+				c.fillStyle = "#ffffff";
+
+				l1++;
+			}
+		}
+	} 
+	main();
 }
+
+//--Failed Stuffs--//
+
+
+// Method.prototype.msort1 = function(){
+// 	// this.status = 1;
+// 	let merge = (arr, l, m, r) =>{
+// 		// let c = [];
+// 		let n1 = m - l + 1;
+// 		let n2 = r - m;
+// 		// console.log(this.data);
+// 		let L = [], R = [];
+// 		for (let i = 0; i < n1; i++)
+// 			L[i] = arr[l + i];
+// 		for (let j = 0; j < n2; j++)
+// 			R[j] = arr[m + 1 + j];
+// 		// let L = this.data.slice(l, m + 1);
+// 		// // console.log(this.data.slice(l, l + n1));
+// 		// let R = this.data.slice(m + 1, r + 1);
+// 		// console.log(L,R);
+// 		let  i = 0, j = 0,k = l;
+// 		while(i < n1 && j < n2){
+// 			if(L[i][1] <= R[j][1]){
+// 				arr[k] = [arr[k][0],L[i][1]];
+// 				// console.log(arr[k][1]);
+// 				// c.push(b.shift());
+// 				i++;
+// 			}
+// 			else{
+// 				arr[k] = [arr[k][0], R[j][1]];
+// 				// c.push(a.shift());
+// 				j++;
+// 			}
+// 			k++;
+// 		}
+// 		while(i < n1){
+// 			arr[k] = [arr[k][0], L[i][1]];
+// 			k++;
+// 			i++;
+// 			// c.push(a.shift());
+// 		}
+// 		while(j < n2){
+// 			arr[k] = [arr[k][0], R[j][1]];
+// 			k++;
+// 			j++;
+// 			// c.push(b.shift());
+// 		}
+		
+		
+// 		this.data = arr;
+// 		this.showData();
+		
+// 		// console.log(arr);
+// 		// return c;
+// 	}
+// 	// let temp_a = this.data;
+// 	let mergesort = (arr, l, r) =>{
+// 		// var m = Math.floor(arr.length / 2)
+// 		// if(arr.length == 1){
+// 		// 	// console.log("sda");
+// 		// 	return arr;
+// 		// }
+
+// 		if(l < r){
+// 			let m = Math.floor(l + (r - l) / 2);
+
+// 			setTimeout(() => {
+// 				mergesort(arr, l, m);
+// 			}, 5);
+// 			setTimeout(() => {
+// 				mergesort(arr, m + 1, r);
+// 			}, 5);
+			
+// 			// console.log(this.data);
+// 			setTimeout(() => {
+// 				merge(arr, l, m, r);
+// 			}, 5);
+			
+// 		}
+// 	} 
+	
+// 	mergesort(this.data,0 , this.data.length - 1);
+	
+// }
